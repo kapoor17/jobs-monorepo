a00bec6dc90cf259d33099d9222442fbb5b3fda1
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-03-15 22:15:54 +0530

    server security packages

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 require('express-async-errors');
+
+const cors = require('cors');
+const helmet = require('helmet');
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit');
 const express = require('express');
 const app = express();
 
@@ -11,7 +16,17 @@ const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
 const authenticate = require('./middleware/authentication');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(cors())
+app.use(helmet());
+app.use(xss())
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticate, jobsRouter);
