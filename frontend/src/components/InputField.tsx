193c96e0de50a3ceca4778ed387eefdf3c4f1a04
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-03-16 23:36:49 +0530

    Layout for no scroll done

--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -3,13 +3,11 @@ import React, { InputHTMLAttributes } from 'react';
 interface IInputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
     label: string,
     onChange?: (value: any) => void
-    className?: string
 }
 
 export const InputField: React.FC<IInputFieldProps> = ({
     name,
     label = "",
-    className = "",
     onChange = () => {},
     ...restProps
 }) => {
@@ -17,7 +15,7 @@ export const InputField: React.FC<IInputFieldProps> = ({
         <div>
             <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
             <input 
-                className={`${className || "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" }`}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 name={name}
                 onChange={(e) => onChange(e.target.value)}
                 {...restProps}
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Navbar } from './';
 import { Outlet } from 'react-router-dom';
+
 export const Layout: React.FC = () => {
     return (
-    <div>
-        <h1>Layout</h1>
-        <Outlet/>
-    </div>
+        <div className='h-screen flex flex-col'>
+            <Navbar/>
+            <section className="bg-gray-50 dark:bg-gray-900 flex-grow">
+                <Outlet/>
+            </section>
+        </div>
     );
 };
