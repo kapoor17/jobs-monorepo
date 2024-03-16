32b83ae4a119bfec68c1ce6f2463c956c8daf283
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-03-17 00:24:36 +0530

    Layout component edit added container components

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -6,9 +6,32 @@ export const Layout: React.FC = () => {
     return (
         <div className='h-screen flex flex-col'>
             <Navbar/>
-            <section className="bg-gray-50 dark:bg-gray-900 flex-grow">
-                <Outlet/>
+            <section className="bg-gray-50 dark:bg-gray-900 flex-grow py-6">
+                <ContainerSm>
+                    <Outlet/>
+                </ContainerSm>
             </section>
         </div>
     );
+};
+
+type ContainerProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export const ContainerSm: React.FC<ContainerProps> = ({children, className}) => {
+  return (
+    <div className={`max-w-6xl w-full mx-auto px-6 ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export const ContainerFull: React.FC<ContainerProps> = ({children, className}) => {
+  return (
+    <div className={`container mx-auto px-6 ${className}`}>
+      {children}
+    </div>
+  );
 };
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ContainerSm } from './Layout';
 
 const NavigationItems = [
     {
@@ -8,24 +9,26 @@ const NavigationItems = [
 export const Navbar: React.FC = () => {
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a  className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Folio: A Jobs API</span>
-                </a>
-                <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button 
-                        className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" 
-                        type="button" 
-                        id="user-menu-button" 
-                        aria-expanded="false" 
-                        data-dropdown-toggle="user-dropdown" 
-                        data-dropdown-placement="bottom"
-                    >
-                        <span className="sr-only">Open user menu</span>
-                        <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo"/>
-                    </button>
+            <ContainerSm>
+                <div className="flex flex-wrap items-center justify-between py-4">
+                    <a  className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Folio: A Jobs API</span>
+                    </a>
+                    <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                        <button 
+                            className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" 
+                            type="button" 
+                            id="user-menu-button" 
+                            aria-expanded="false" 
+                            data-dropdown-toggle="user-dropdown" 
+                            data-dropdown-placement="bottom"
+                        >
+                            <span className="sr-only">Open user menu</span>
+                            <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo"/>
+                        </button>
+                    </div>
                 </div>
-            </div>
+            </ContainerSm>
         </nav>
     );
 };
